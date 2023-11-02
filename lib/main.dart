a40a4475810e79ade31738bd51206d2c7640e4ad
Sujin S <57178220+BornAsSlayer@users.noreply.github.com>
2023-11-02 08:12:49 +0530

    battery charging state is also shown now

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,7 @@ class _HomeState extends State<Home> {
             ),
             const TurnOnSwitch(),
             const BatteryPercentageText(),
+            const BatteryStatusText(),
           ],
         )
       ),
@@ -126,6 +127,50 @@ class _BatteryPercentageTextState extends State<BatteryPercentageText> {
           color: Colors.grey,
           fontFamily: 'SomeTypeMono',
         ),
+        )
+      ],
+    );
+  }
+}
+
+class BatteryStatusText extends StatefulWidget {
+  const BatteryStatusText({super.key});
+
+  @override
+  State<BatteryStatusText> createState() => _BatteryStatusTextState();
+}
+
+class _BatteryStatusTextState extends State<BatteryStatusText> {
+  final Battery battery = Battery();
+  String batteryStatus = "battery status is shown here";
+
+  BatteryState? batteryState;
+  StreamSubscription <BatteryState>? batteryStateSubscription;
+
+  void updateBatteryState(BatteryState state){
+    if(batteryState == state) return;
+    setState(() {
+      batteryState = state;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    battery.batteryState.then(updateBatteryState);
+    batteryStateSubscription = battery.onBatteryStateChanged.listen(updateBatteryState);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('$batteryState',style: const TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey,
+          fontFamily: 'SomeTypeMono',
+        )
         )
       ],
     );
